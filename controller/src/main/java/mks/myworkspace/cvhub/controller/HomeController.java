0e84afbf3e6e0d48eebf5831a63ab034f4e6f5c7
nnhaute <145630613+nnhaute@users.noreply.github.com>
2024-09-11 19:03:12 +0700

    modify header.css

--- controller/src/main/java/mks/myworkspace/cvhub/controller/HomeController.java
+++ controller/src/main/java/mks/myworkspace/cvhub/controller/HomeController.java
@@ -18,6 +18,7 @@
  */
 
 package mks.myworkspace.cvhub.controller;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -45,99 +46,95 @@ import mks.myworkspace.cvhub.service.LocationService;
 @Controller
 
 public class HomeController extends BaseController {
-	
+
 	@Autowired
 	JobRoleService jobRoleService;
 	@Autowired
 	LocationService locationService;
-	
+
 	/**
-     * This method is called when binding the HTTP parameter to bean (or model).
-     * 
-     * @param binder
-     */
-    @InitBinder
-    protected void initBinder(WebDataBinder binder) {
-        // Sample init of Custom Editor
+	 * This method is called when binding the HTTP parameter to bean (or model).
+	 * 
+	 * @param binder
+	 */
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) {
+		// Sample init of Custom Editor
 
 //        Class<List<ItemKine>> collectionType = (Class<List<ItemKine>>)(Class<?>)List.class;
 //        PropertyEditor orderNoteEditor = new MotionRuleEditor(collectionType);
 //        binder.registerCustomEditor((Class<List<ItemKine>>)(Class<?>)List.class, orderNoteEditor);
 
-    }
-    
+	}
+
 	/**
 	 * Simply selects the home view to render by returning its name.
-     * @return 
+	 * 
+	 * @return
 	 */
-	@RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
+	@RequestMapping(value = { "/", "/home" }, method = RequestMethod.GET)
 	public ModelAndView displayHome(HttpServletRequest request, HttpSession httpSession) {
 		ModelAndView mav = new ModelAndView("home");
 
 		initSession(request, httpSession);
-		
+
 		mav.addObject("currentSiteId", getCurrentSiteId());
 		mav.addObject("userDisplayName", getCurrentUserDisplayName());
 		return mav;
 	}
-	@RequestMapping(value = {"/add"}, method = RequestMethod.GET)
-    public ModelAndView addJobRoles() {
-		 ModelAndView mav = new ModelAndView("searchResult");
-		List<Location> locations=locationService.getRepo().findAll();
-		List<JobRole> jobRoles = Arrays.asList(
-            new JobRole(1L,"Java Developer", locations.get(0), "IT"),
-            new JobRole(2L,"Marketing Specialist",locations.get(0), "Marketing"),
-            new JobRole(3L,"Financial Analyst", locations.get(0), "Finance"),
-            new JobRole(4L,"Registered Nurse", locations.get(0), "Healthcare"),
-            new JobRole(5L,"Civil Engineer", locations.get(0), "Construction"),
-            new JobRole(6L,"Software Engineer", locations.get(0), "IT"),
-            new JobRole(7L,"Data Scientist", locations.get(0), "IT"),
-            new JobRole(8L,"Project Manager",locations.get(0), "Construction"),
-            new JobRole(9L,"Marketing Manager",locations.get(0), "Marketing"),
-            new JobRole(10L,"Accountant", locations.get(0), "Finance"),
-            new JobRole(11L,"Teacher", locations.get(0), "Education"),
-            new JobRole(12L,"Physician", locations.get(0), "Healthcare")
-        );
-        jobRoleService.getRepo().saveAll(jobRoles);
 
-        return mav;
-    }
-	@RequestMapping(value = {"/addLocation"}, method = RequestMethod.GET)
-    public ModelAndView addLocation() {
-		 ModelAndView mav = new ModelAndView("searchResult");
+	@RequestMapping(value = { "/add" }, method = RequestMethod.GET)
+	public ModelAndView addJobRoles() {
+		ModelAndView mav = new ModelAndView("searchResult");
+		List<Location> locations = locationService.getRepo().findAll();
+		List<JobRole> jobRoles = Arrays.asList(new JobRole(1L, "Java Developer", locations.get(0), "IT"),
+				new JobRole(2L, "Marketing Specialist", locations.get(0), "Marketing"),
+				new JobRole(3L, "Financial Analyst", locations.get(0), "Finance"),
+				new JobRole(4L, "Registered Nurse", locations.get(0), "Healthcare"),
+				new JobRole(5L, "Civil Engineer", locations.get(0), "Construction"),
+				new JobRole(6L, "Software Engineer", locations.get(0), "IT"),
+				new JobRole(7L, "Data Scientist", locations.get(0), "IT"),
+				new JobRole(8L, "Project Manager", locations.get(0), "Construction"),
+				new JobRole(9L, "Marketing Manager", locations.get(0), "Marketing"),
+				new JobRole(10L, "Accountant", locations.get(0), "Finance"),
+				new JobRole(11L, "Teacher", locations.get(0), "Education"),
+				new JobRole(12L, "Physician", locations.get(0), "Healthcare"));
+		jobRoleService.getRepo().saveAll(jobRoles);
+
+		return mav;
+	}
+
+	@RequestMapping(value = { "/addLocation" }, method = RequestMethod.GET)
+	public ModelAndView addLocation() {
+		ModelAndView mav = new ModelAndView("searchResult");
 		List<Location> locations = locationService.fetchLocationsFromAPI();
-        
-        locationService.getRepo().saveAll(locations);
 
-        return mav;
-    }
+		locationService.getRepo().saveAll(locations);
+
+		return mav;
+	}
+
 	@RequestMapping(value = "/search", method = RequestMethod.GET)
-	public ModelAndView searchJobs(
-	    @RequestParam(value = "keyword", required = false, defaultValue = "") String keyword,
-	    @RequestParam(value = "location", required = false, defaultValue = "0") int locationCode,
-	    @RequestParam(value = "industry", required = false, defaultValue = "") String industry,
-	    HttpServletRequest request, 
-	    HttpSession httpSession
-	) {  
-	    ModelAndView mav = new ModelAndView("searchResult");
+	public ModelAndView searchJobs(@RequestParam(value = "keyword", required = false, defaultValue = "") String keyword,
+			@RequestParam(value = "location", required = false, defaultValue = "0") int locationCode,
+			@RequestParam(value = "industry", required = false, defaultValue = "") String industry,
+			HttpServletRequest request, HttpSession httpSession) {
+		ModelAndView mav = new ModelAndView("searchResult");
 
-	    // 1. Trước tiên, lọc theo ngành
-	    List<JobRole> jobs = jobRoleService.getRepo().findByIndustry(industry);
+		// 1. Trước tiên, lọc theo ngành
+		List<JobRole> jobs = jobRoleService.getRepo().findByIndustry(industry);
 
-	    // 2. Tiếp tục lọc kết quả dựa trên các tiêu chí khác
-	    List<JobRole> searchResults = jobs.stream()
-	        .filter(job -> 
-	            job.getTitle().toLowerCase().contains(keyword.toLowerCase()) &&
-	            (locationCode == 0 || job.getLocation().getCode() == locationCode)
-	        )
-	        .collect(Collectors.toList());
+		// 2. Tiếp tục lọc kết quả dựa trên các tiêu chí khác
+		List<JobRole> searchResults = jobs.stream()
+				.filter(job -> job.getTitle().toLowerCase().contains(keyword.toLowerCase())
+						&& (locationCode == 0 || job.getLocation().getCode() == locationCode))
+				.collect(Collectors.toList());
 
-	    mav.addObject("searchResults", searchResults);
-	    mav.addObject("keyword", keyword);
-	    mav.addObject("location", locationCode);
-	    mav.addObject("industry", industry); 
+		mav.addObject("searchResults", searchResults);
+		mav.addObject("keyword", keyword);
+		mav.addObject("location", locationCode);
+		mav.addObject("industry", industry);
 
-	    return mav;
+		return mav;
 	}
 }
-	
