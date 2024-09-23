29a471fd47501f2cfc1b1d4a7b3ff0b6e9bc959c
nnhaute <145630613+nnhaute@users.noreply.github.com>
2024-09-23 23:30:55 +0700

    Display Detail Job
    In JobRequestController:
    Get Detail Job and display to detailJob.html

--- jpa/src/main/java/mks/myworkspace/cvhub/repository/JobRequestRepository.java
+++ jpa/src/main/java/mks/myworkspace/cvhub/repository/JobRequestRepository.java
@@ -1,9 +1,12 @@
 package mks.myworkspace.cvhub.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import mks.myworkspace.cvhub.entity.JobRequest;
+import org.springframework.data.domain.*;
 
+@Repository
 public interface JobRequestRepository extends JpaRepository<JobRequest, Long> {
-
+	Page<JobRequest> findAll(Pageable pageable);
 }
