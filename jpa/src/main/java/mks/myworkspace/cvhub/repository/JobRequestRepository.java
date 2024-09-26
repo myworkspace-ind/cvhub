0619d67033003191669168ce1e0e3fed8f104f64
nnhaute <145630613+nnhaute@users.noreply.github.com>
2024-09-26 14:52:38 +0700

    Update page home to display all job
    update button view detail

--- jpa/src/main/java/mks/myworkspace/cvhub/repository/JobRequestRepository.java
+++ jpa/src/main/java/mks/myworkspace/cvhub/repository/JobRequestRepository.java
@@ -1,27 +1,20 @@
 package mks.myworkspace.cvhub.repository;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
-import org.springframework.stereotype.Repository;
-
-import mks.myworkspace.cvhub.entity.JobRequest;
-import org.springframework.data.domain.*;
-=======
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
 
 import mks.myworkspace.cvhub.entity.JobRequest;
-import mks.myworkspace.cvhub.entity.Organization;
->>>>>>> remotes/origin/dev_minh
 
-@Repository
 public interface JobRequestRepository extends JpaRepository<JobRequest, Long> {
-<<<<<<< HEAD
-	Page<JobRequest> findAll(Pageable pageable);
-=======
 	  @Query("SELECT jr FROM JobRequest jr WHERE jr.organization.id = :organizationId")
 	    List<JobRequest> findByOrganizationId(@Param("organizationId") Long organizationId);
->>>>>>> remotes/origin/dev_minh
+//	  @Query("SELECT jr FROM JobRequest jr WHERE jr.id = :id")
+	    Optional<JobRequest> findById(@Param("id") Long id);
+	    Page<JobRequest> findAll(@Param("pageRequest") Pageable pageRequest );
 }
