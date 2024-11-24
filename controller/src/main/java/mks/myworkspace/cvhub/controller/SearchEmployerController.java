2d1d241077416a0ea5dc18b60a751061d62badf9
Hùng Nguyễn <vanhung211104@gmail.com>
2024-11-24 15:37:59 +0700

    Task anh Thạch giao: Code màn hình tìm kiếm Employer. (Mở rộng: hiển thị số lượng trong trang hiện tại, không hiển thị tất cả lên để phân trang)

--- controller/src/main/java/mks/myworkspace/cvhub/controller/SearchEmployerController.java
+++ controller/src/main/java/mks/myworkspace/cvhub/controller/SearchEmployerController.java
@@ -6,6 +6,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,39 +16,76 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-
 import mks.myworkspace.cvhub.entity.Organization;
+import mks.myworkspace.cvhub.repository.OrganizationRepository;
 import mks.myworkspace.cvhub.service.OrganizationService;
 
 @Controller
-public class SearchEmployerController {
+public class SearchEmployerController extends BaseController{
 
     @Autowired
     OrganizationService organizationService;
+    @Autowired
+    OrganizationRepository organizationRepository;
     
     @GetMapping("/resources")
-    public ModelAndView getEmployers(HttpServletRequest request, HttpSession httpSession){
+    public ModelAndView getEmployers(HttpServletRequest request,
+						    		HttpSession httpSession, 
+						    		@RequestParam(value = "page", defaultValue = "0") int page,
+						            @RequestParam(value ="limit", defaultValue = "10") int limit){
     	ModelAndView mav = new ModelAndView("employer");
     	
-    	List<Organization> employers = organizationService.getRepo().findAll();
-    	
+    	initSession(request, httpSession);
+		PageRequest pageRequest = PageRequest.of(
+                page, limit,
+                Sort.by("createdDate").descending()
+        );
+//    	List<Organization> employers = organizationService.getRepo().findAll();
+        Page<Organization> employersPage = organizationRepository.findAll(pageRequest);
+        int totalPages = employersPage.getTotalPages();
+        List<Organization> employers = employersPage.getContent();
+        
+        mav.addObject("totalPages", totalPages);
+        mav.addObject("currentPage", page);
+        mav.addObject("limit", limit);
+		mav.addObject("currentSiteId", getCurrentSiteId());
+		mav.addObject("userDisplayName", getCurrentUserDisplayName());
     	mav.addObject("employers", employers);
     	
     	return mav;
     }
     
     @RequestMapping(value = "/search-employer", method = RequestMethod.GET)
-	public ModelAndView searchJobs(@RequestParam String keyword, HttpServletRequest request,
-			HttpSession httpSession) {
-		ModelAndView mav = new ModelAndView("employer");
+	public ModelAndView searchJobs( HttpServletRequest request,
+									HttpSession httpSession, 
+									@RequestParam String keyword,
+									@RequestParam(value = "page", defaultValue = "0") int page,
+							        @RequestParam(value ="limit", defaultValue = "10") int limit) {
+		ModelAndView mav = new ModelAndView("searchEmployer");
 		
-		List<Organization> searchResults = organizationService.findByTitleContaining(keyword);
+//		List<Organization> searchResults = organizationService.findByTitleContaining(keyword);
 		
-		System.out.println(searchResults.size());
-		mav.addObject("employers", searchResults);
+		PageRequest pageRequest = PageRequest.of(
+                page, limit,
+                Sort.by("createdDate").descending()
+        );
+		
+		Page<Organization> employersPage = organizationRepository.findByTitleContaining(pageRequest, keyword);
+        int totalPages = employersPage.getTotalPages();
+        List<Organization> searchResult = employersPage.getContent();
+        
+        mav.addObject("totalPages", totalPages);
+        mav.addObject("currentPage", page);
+        mav.addObject("limit", limit);
+        mav.addObject("keyword", keyword);
+		mav.addObject("currentSiteId", getCurrentSiteId());
+		mav.addObject("userDisplayName", getCurrentUserDisplayName());
+    	mav.addObject("employers", searchResult);
 
 		return mav;
 	}
+    
+    
     
     
 }
