4da74f55d10aa5f97a8bcd97b0da16533b04a801
HaoMilk <152190820+HaoMilk@users.noreply.github.com>
2024-11-24 14:53:38 +0700

    Revert "Dev kinhao"

--- biz/src/main/java/mks/myworkspace/cvhub/service/SearchJobService.java
+++
@@ -1,9 +0,0 @@
-package mks.myworkspace.cvhub.service;
-
-import java.util.List;
-
-import mks.myworkspace.cvhub.entity.JobRequest;
-
-public interface SearchJobService {
- List<JobRequest> searchJobRequest(String keyword,int locationCD,Long industryCD);
-}
