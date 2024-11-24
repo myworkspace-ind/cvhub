5df250e6af038079495df06126e3ae3896299b76
vophuhao <vophuhao2004@gmail.com>
2024-11-24 15:26:43 +0700

    .

--- biz/src/main/java/mks/myworkspace/cvhub/service/impl/FolderUserImpl.java
+++ biz/src/main/java/mks/myworkspace/cvhub/service/impl/FolderUserImpl.java
@@ -1,6 +1,8 @@
 package mks.myworkspace.cvhub.service.impl;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -20,25 +22,42 @@ import mks.myworkspace.cvhub.service.FolderUserService;
 @Service
 public class FolderUserImpl  implements FolderUserService {
 
-	 private static final String ROOT_DIR = "src/main/webapp/resources/static/userCV";
+	 private static final String ROOT_DIR = "../../../../../../../../web/src/main/webapp/resources/";
 	
+	 @Autowired
+	 private ResourceLoader resourceLoader;
+	 
+	 
+	 
 
 	    @Override
 	    public List<String> getUserFolders() {
+	    	  Resource resource = resourceLoader.getResource("classpath:main/webapp/resources/userCV");
+
+	          try {
+	              File file = resource.getFile();
+	              System.out.println("File path: " + file.getAbsolutePath());
+	          } catch (IOException e) {
+	              System.out.println("Không tìm thấy tệp: " + e.getMessage());
+	          }
+	    	
 	    	
 	    	File rootFolder = new File(ROOT_DIR);
-
+	    	
+	        
 	    	if (!rootFolder.exists()) {
+	    	
 	    	    rootFolder.mkdirs(); // Tạo thư mục gốc nếu chưa tồn tại
+	    	   
 	    	}
-
+	    	
 	    	// Lấy danh sách thư mục con
 	    	File[] directories = rootFolder.listFiles(File::isDirectory);
-
-//	    	if (directories == null || directories.length == 0) {
-//	    	    System.out.println("Không tìm thấy thư mục con nào.");
-//	    	    return Collections.emptyList();
-//	    	}
+	    
+	    	if (directories == null || directories.length == 0) {
+	    		System.out.println(Arrays.toString(directories));
+	    	   
+	    	}
 
 	    	// Chuyển danh sách thư mục thành danh sách tên
 	    	List<String> folderNames = Arrays.stream(directories)
