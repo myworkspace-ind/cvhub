abf29dd75947bcaf0e9562df2bbe6b04f5772a24
Nguyễn Đức Tây <129637919+taaytungstieenf@users.noreply.github.com>
2024-11-26 18:20:08 +0700

    handle the creation of the DocumentBuilder and parsing logic separately

--- common/src/main/java/mks/myworkspace/cvhub/common/AppUtility.java
+++ common/src/main/java/mks/myworkspace/cvhub/common/AppUtility.java
@@ -21,48 +21,72 @@ package mks.myworkspace.cvhub.common;
 
 import java.io.File;
 import java.io.IOException;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import lombok.extern.slf4j.Slf4j;
-
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
-import mks.myworkspace.cvhub.common.AppUtility;
-
 /**
- * An example Utility.
+ * Utility class for common XML-related operations.
  * 
  * @author Thach Ngoc Le (ThachLN@mks.com.vn)
- *
  */
 @Slf4j
 public class AppUtility {
 
     /**
-     * Parse a XML File into a document model.
-     * @param xmlFile file of xml.
-     * @return Document model if no error.
+     * Parse a XML file into a Document object.
+     * 
+     * @param xmlFile The XML file to be parsed.
+     * @return Document object or null if there is an error during parsing.
      */
     public static Document parseXML(File xmlFile) {
-        Document xmlDoc = null;
-
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        DocumentBuilder db;
-
-        try {
-            db = dbf.newDocumentBuilder();
-            xmlDoc = db.parse(xmlFile);
-        } catch (ParserConfigurationException ex) {
-            log.error("Could not parse the file.", ex);
-        } catch (SAXException ex) {
-        	log.error("Error in XML file.", ex);
-        } catch (IOException ex) {
-        	log.error("Could not open file.", ex);
+        if (xmlFile == null || !xmlFile.exists()) {
+            log.error("XML file is null or does not exist.");
+            return null;
         }
-        
-        return xmlDoc;
+
+        DocumentBuilder db = createDocumentBuilder();
+        if (db == null) {
+            return null;
+        }
+
+        return parseWithBuilder(db, xmlFile);
+    }
+
+    /**
+     * Creates a new DocumentBuilder instance.
+     * 
+     * @return A DocumentBuilder instance or null if creation fails.
+     */
+    private static DocumentBuilder createDocumentBuilder() {
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            return dbf.newDocumentBuilder();
+        } catch (ParserConfigurationException ex) {
+            log.error("Could not create DocumentBuilder.", ex);
+            return null;
+        }
+    }
+
+    /**
+     * Parse the XML file using the provided DocumentBuilder.
+     * 
+     * @param db       The DocumentBuilder instance.
+     * @param xmlFile  The XML file to be parsed.
+     * @return Document object or null if an error occurs.
+     */
+    private static Document parseWithBuilder(DocumentBuilder db, File xmlFile) {
+        try {
+            log.info("Parsing XML file: {}", xmlFile.getAbsolutePath());
+            return db.parse(xmlFile);
+        } catch (SAXException ex) {
+            log.error("Error during XML parsing.", ex);
+        } catch (IOException ex) {
+            log.error("Error opening or reading the XML file.", ex);
+        }
+        return null;
     }
 }
