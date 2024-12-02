69e9663d4e770afdd9ce5b4271065c588d22b403
binh1922004 <binh1920042018@gmail.com>
2024-12-02 13:46:36 +0700

    fix loi

--- jpa/src/main/java/mks/myworkspace/cvhub/repository/JobApplicationRepository.java
+++ jpa/src/main/java/mks/myworkspace/cvhub/repository/JobApplicationRepository.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -45,5 +44,5 @@ public interface JobApplicationRepository extends JpaRepository<JobApplication, 
     Page<JobApplication> findByCreatedDateBetween(
             @Param("startDate") Date startDate,
             @Param("endDate") Date endDate,
-            @Param("pageRequest") PageRequest pageable);
+            @Param("pageRequest") org.springframework.data.domain.Pageable pageable);
 }
