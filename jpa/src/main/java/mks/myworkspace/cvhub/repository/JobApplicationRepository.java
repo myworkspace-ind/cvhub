6bc45076b0c78a6c1b6103c636b6d813a22ede20
vophuhao <vophuhao2004@gmail.com>
2024-12-02 09:37:14 +0700

    merge to develop

--- jpa/src/main/java/mks/myworkspace/cvhub/repository/JobApplicationRepository.java
+++ jpa/src/main/java/mks/myworkspace/cvhub/repository/JobApplicationRepository.java
@@ -3,6 +3,7 @@ package mks.myworkspace.cvhub.repository;
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +12,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import mks.myworkspace.cvhub.entity.CV;
 import mks.myworkspace.cvhub.entity.JobApplication;
 import mks.myworkspace.cvhub.entity.JobRequest;
 import mks.myworkspace.cvhub.entity.User;
@@ -18,9 +20,10 @@ import mks.myworkspace.cvhub.entity.User;
 @Repository
 public interface JobApplicationRepository extends JpaRepository<JobApplication, Long> {
 	@Query("SELECT ja FROM JobApplication ja WHERE ja.user = :user")
-    List<JobApplication> findByUser(@Param("user") User user);
-	
+	List<JobApplication> findByUser(@Param("user") User user);
+
 	@Query("SELECT ja FROM JobApplication ja WHERE ja.jobRequest = :jobRequest")
+<<<<<<< HEAD
         List<JobApplication> findByUserAndJobRequest(@Param("jobRequest") JobRequest jobRequest);
         Page<JobApplication> findAll(@Param("pageRequest") Pageable pageRequest );
 
@@ -34,4 +37,18 @@ public interface JobApplicationRepository extends JpaRepository<JobApplication, 
         List<JobApplication> findByCreatedDateBetween(
                 @Param("startDate") Date startDate,
                 @Param("endDate") Date endDate);
+=======
+	List<JobApplication> findByUserAndJobRequest(@Param("jobRequest") JobRequest jobRequest);
+
+	@Query("SELECT ja FROM JobApplication ja WHERE ja.id = :id")
+	Optional<JobApplication> findByJobApplicationId(@Param("id") Long id);
+
+	@Query("SELECT ja FROM JobApplication ja " +
+		       "JOIN ja.jobRequest jr " +
+		       "WHERE ja.status = :status " +
+		       "AND jr.organization.id = :organizationId")
+	List<JobApplication> findJobApplicationByStatusAndOrganizationId(
+		    @Param("status") String status, 
+		    @Param("organizationId") Long organizationId);
+>>>>>>> remotes/origin/develop
 }
