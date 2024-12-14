c2a06cc9b2766d34d95647c8a0eeadb30be0d446
trinh318 <htttrinh145@gmail.com>
2024-12-14 07:20:39 +0700

    update saved job

---
+++ biz/src/main/java/mks/myworkspace/cvhub/service/JobSavedService.java
@@ -0,0 +1,25 @@
+package mks.myworkspace.cvhub.service;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+import mks.myworkspace.cvhub.entity.JobSaved;
+import mks.myworkspace.cvhub.entity.JobRequest;
+import mks.myworkspace.cvhub.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+public interface JobSavedService {
+	void AddJobSaved(User user, JobRequest jobRequest);
+
+	List<JobSaved> getJobSavedByUser(User currentUser);
+	boolean hasJobSaved(User user, Long jobRequestId);
+	List<JobSaved> getJobSavedByJobRequest(JobRequest jobRequest);
+	List<JobSaved> findAll();
+	Page<JobSaved> findAll(PageRequest pageRequest);
+	Page<JobSaved> findByCreatedDateBetween(Date start, Date end, PageRequest pageRequest);
+	List<JobSaved> findByCreatedDateBetween(Date start, Date end);
+	void deleteJobSavedById(Long id);
+	JobSaved getJobSavedByJobSavedId(Long id);
+}
